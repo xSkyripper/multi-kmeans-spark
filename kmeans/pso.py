558a5a0062bd4da79d5607c0267bc7c2e45f6c03
Lila Ciprian LAZAR <clazar@ias.bitdefender.biz>
2018-06-03 22:20:36 +0300

    Add KDTree

--- kmeans/pso.py
+++ kmeans/pso.py
@@ -3,6 +3,7 @@ import numpy as np
 import math
 import heapq
 
+from sklearn.neighbors import NearestNeighbors
 from pyspark.sql import SparkSession
 from pprint import pprint
 
@@ -26,18 +27,10 @@ def get_ns_neighbors(distances, ns):
     return sorted(max_heap)
 
 
-def compute_nearest_neighbors(data_items, bc_data_items, ns):
-    zipped = data_items.zipWithIndex().map(lambda x: (x[1], x[0]))
-    crossed = zipped.map(lambda x: (x, bc_data_items.value))
-    flattened = crossed.flatMap(lambda p_points: ((p_points[0], x) for x in p_points[1]))
-    filtered = flattened.filter(lambda x: x[0][0] != x[1][0])
-    distances = filtered.map(lambda x1_x2: (x1_x2[0], x1_x2[1], euclidean_distance(x1_x2[0][1], x1_x2[1][1])))
-    indexes_distances = distances.map(lambda x: (x[0][0], (x[2], x[1][0])))
-    grouped = indexes_distances.groupByKey().map(lambda x: (x[0], list(x[1])))
-    ns_neighbors = grouped.map(lambda point_dists: (point_dists[0], get_ns_neighbors(point_dists[1], ns)))
-    ns_neighbors_indexes = ns_neighbors.map(lambda x: (x[0], [i[1] for i in x[1]]))
-
-    return ns_neighbors_indexes
+def compute_nearest_neighbors(bc_data_items, nrbs, ns):
+    bc_data_items = bc_data_items.value
+    ns_neighbour = map(lambda x: (x[0], nrbs.kneighbors(X=[x[1]], return_distance=False)[0][1:]), bc_data_items)
+    return list(ns_neighbour)
 
 
 @click.command()
@@ -50,17 +43,24 @@ def main(file, no_clusters, max_iterations, itr):
     lines = spark.read.text(file).rdd.map(lambda r: r[0])
     data_items = lines.map(parse_vector).cache()
     bc_data_items = spark.sparkContext.broadcast(
-        data_items.zipWithIndex().map(lambda x: (x[1], x[0]))
-        .collect()
+        data_items
+            .zipWithIndex()
+            .map(lambda x: (x[1], x[0]))
+            .collect()
     )
     n = data_items.count()
     # ns = compute_ns(n, no_clusters)
     ns = 2
+    nrbs = NearestNeighbors(n_neighbors=ns + 1, algorithm="kd_tree")
+    nrbs.fit(data_items.collect())
+    spark.sparkContext.broadcast(nrbs)
 
-    nearest_neighbors = compute_nearest_neighbors(data_items, bc_data_items, ns)
-    bc_nearest_neighbors = spark.sparkContext.broadcast(nearest_neighbors.collect())
+    nearest_neighbors = compute_nearest_neighbors(bc_data_items, nrbs, ns)
+    bc_nearest_neighbors = spark.sparkContext.broadcast(nearest_neighbors)
     print('Nearest neighbors')
-    pprint(bc_nearest_neighbors.value)
+    with open("out.txt", mode="wt") as file_object:
+        for row in bc_nearest_neighbors.value:
+            file_object.write("{}\n".format(str(row)))
 
 
 if __name__ == '__main__':
