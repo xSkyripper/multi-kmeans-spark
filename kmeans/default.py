43cdc5477e8a2ffe0db2dd71d74b1f9897e581b5
xSkyripper <alexandru.cristea604@gmail.com>
2018-06-04 00:13:45 +0300

    kmeans: more fixes

--- kmeans/default.py
+++ kmeans/default.py
@@ -8,35 +8,34 @@ def parse_vector(line):
     return np.array([float(x) for x in line.split(' ')])
 
 
-def closest_point(point, centroids):
-    best_index = 0
-    closest = float('+inf')
+def euclidean_distance(x1, x2):
+    return np.linalg.norm(x1 - x2)
 
-    for i in range(0, len(centroids)):
-        temp_dist = np.sum((point - centroids[i]) ** 2)
-        if temp_dist < closest:
-            closest = temp_dist
-            best_index = i
+
+def closest_point(point, centroids, k):
+    best_index = 0
+    smallest_dist = np.inf
+
+    for idx in range(k):
+        temp_dist = euclidean_distance(point - centroids[idx])
+        if temp_dist < smallest_dist:
+            smallest_dist = temp_dist
+            best_index = idx
 
     return best_index
 
 
-@click.command()
-@click.option('-f', '--file', required=True)
-@click.option('-k', '--no-clusters', required=True, type=click.INT)
-@click.option('-c', '--converge-dist', required=True, type=click.FLOAT)
-def main(file, no_clusters, converge_dist):
+def default_kmeans(input_file, k, convergence_dist):
     spark = SparkSession.builder.appName('PythonKMeans').getOrCreate()
-    lines = spark.read.text(file).rdd.map(lambda r: r[0])
+    lines = spark.read.text(input_file).rdd.map(lambda r: r[0])
     data_items = lines.map(parse_vector).cache()
-
-    centroids = data_items.takeSample(False, no_clusters)
+    centroids = data_items.takeSample(False, k)
 
     iterations = 0
     centroids_delta_dist = 1.0
 
-    while centroids_delta_dist > converge_dist:
-        closest = data_items.map(lambda p: (closest_point(p, centroids), (p, 1)))
+    while centroids_delta_dist > convergence_dist:
+        closest = data_items.map(lambda p: (closest_point(p, centroids, k), (p, 1)))
         point_stats = closest.reduceByKey(lambda p1_c1, p2_c2: (p1_c1[0] + p2_c2[0], p1_c1[1] + p2_c2[1]))
         new_points = point_stats.map(lambda st: (st[0], st[1][0] / st[1][1])).collect()
 
@@ -50,6 +49,14 @@ def main(file, no_clusters, converge_dist):
     print("Final centroids in {} iterations:".format(iterations))
     pprint(centroids)
     spark.stop()
+
+
+@click.command()
+@click.option('-f', '--file', required=True)
+@click.option('-k', '--no-clusters', required=True, type=click.INT)
+@click.option('-c', '--convergence-dist', required=True, type=click.FLOAT)
+def main(file, no_clusters, convergence_dist):
+    default_kmeans(file, no_clusters, convergence_dist)
 
 
 if __name__ == '__main__':
