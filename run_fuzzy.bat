47a6e9c05ac54577cc6f76166f6ff5305d8cfb5c
Lila Ciprian LAZAR <clazar@ias.bitdefender.biz>
2018-06-10 00:04:42 +0300

    Modify run files

--- run_fuzzy.bat
+++ run_fuzzy.bat
@@ -1,1 +1,4 @@
-"D:\Python Envs\Python3\SparkEnv\Scripts\python.exe" -m kmeans.fuzzy -f tmp/in.txt -k 2 -c 0.01 -m 1.25
+"D:\Python Envs\Python3\SparkEnv\Scripts\python.exe" -m kmeans.fuzzy -f tmp\ds_mlbook_small.txt -k 5  -c 0.01 -m 1.25
+
+:: tmp\ds_mlbook_small.txt
+:: dataset\HTRU_2.csv
